8c05d862d887fd1f5cfaac563dc20a113af205ef
Chinhaa <dinhvanchinh1706@gmail.com>
2026-01-29 22:12:44 +0700

    Autoplw

---
+++ my_test.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False) 
+
+    page = browser.new_page()
+    page.goto('https://www.youtube.com/watch?v=MfISy9zei90')
+    page.wait_for_timeout(10_000)
